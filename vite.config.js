618a5243f01d233123839df7d2599d5f9aa6b498
Maojin-juan <edwardelric199887@gmail.com>
2024-07-19 16:23:35 +0800

    解決navbarNews的段點出現問題，謝謝copilot。

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
         glob
           .sync('src/pages/**/*.html')
           .map((file) => [
-            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
+            path.relative('src', file.slice(0, file.length - path.extname(file).length)),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
